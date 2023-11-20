b0b1e821894167c0c7b99dd3eb008bb4fce28acf
kuuku123 <dohyun0047@gmail.com>
2023-11-20 16:53:15 +0900

    init

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 10
+#define HEIGHT 10
+
+char level[] = " .-=coA@#";
+#define level_count sizeof(level)/sizeof(level[0]-1) // because string in c comes with 0 at last , making size bigger
+
+float grid[HEIGHT][WIDTH] = {0};
+
+float rand_float(void) 
+{
+    return (float)rand() /(float)RAND_MAX;
+}
+
+int main(void)
+{
+
+    srand(time(0));
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        for (size_t x = 0; x < WIDTH; ++x) {
+            grid[y][x] = rand_float();
+        }
+    }
+
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        for (size_t x = 0; x < WIDTH; ++x) {
+            char x = level[(int)grid[y][x] *(level_count-1)];
+
+            grid[y][x];
+            printf("%f ", grid[y][x]);
+        }
+    }
+    return 0;
+}
